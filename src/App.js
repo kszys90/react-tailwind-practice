3d9a9257e8fb6315ec10d5f914a854bbe7cc7763
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-03-03 19:47:35 +0100

    Craeted routes and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Profile" element={<Profile />}></Route>
+          <Route path="/Offers" element={<Offers />}></Route>
+          <Route path="/SignIn" element={<SignIn />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/ForgotPassword" element={<ForgotPassword />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
