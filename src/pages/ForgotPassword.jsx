7548c30c57be6bbffd65c2c93512bca97b1857ea
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-03-03 21:51:00 +0100

    items-center added to sign-in, out and forgot pass

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -10,7 +10,7 @@ export default function ForgotPassword() {
   return (
     <section>
         <h1 className='text-3xl text-center mt-6 mb-20 font-bold'>Forgot Password</h1>
-        <div className='flex justify-center flex-wrap'>
+        <div className='flex justify-center items-center flex-wrap'>
           <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6 items-center px-6 max-w-6xl'>
             <img className='w-full rounded-2xl'
             src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
