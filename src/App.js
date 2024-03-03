3c67065e3a0ee0148b2bd0cf5266980f1a56760c
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-03-03 20:09:02 +0100

    created header component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Offers from './pages/Offers';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/Profile" element={<Profile />}></Route>
           <Route path="/Offers" element={<Offers />}></Route>
-          <Route path="/SignIn" element={<SignIn />}></Route>
-          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/ForgotPassword" element={<ForgotPassword />}></Route>
         </Routes>
       </Router>
